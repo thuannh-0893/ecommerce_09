0737d1e05a3fd0ba4cac0f0266ff3b907da7c6ea
nhattvcn-0892 <truong.van.cong.nhat@sun-asterisk.com>
2019-05-30 16:09:43 +0700

    Initialize project

---
+++ config/application.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Ecommerce
+  class Application < Rails::Application
+    Bundler.require(*Rails.groups)
+    config.time_zone = Settings.time_zone
+    config.load_defaults 5.2
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
+    config.i18n.available_locales = [:en, :vi]
+    config.i18n.default_locale = :en
+  end
+end
