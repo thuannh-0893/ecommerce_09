b9e763966f8ecfc84b5fac2bb5fc94ff630c6e75
nhattvcn-0892 <truong.van.cong.nhat@sun-asterisk.com>
2019-05-31 14:01:07 +0700

    12879 - Login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
   before_action :set_locale
 
   private
@@ -12,5 +13,25 @@ class ApplicationController < ActionController::Base
 
   def default_url_options
     {locale: I18n.locale}
+  end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t "helpers.warning[login]"
+    redirect_to login_url
+  end
+
+  def correct_user
+    find_user
+    return if current_user?(@user) || current_user.admin?
+    flash[:danger] = t "helpers.error[not_correct_user]"
+    redirect_to root_url
+  end
+
+  def admin_user
+    return if current_user.admin?
+    flash[:danger] = t "helpers.error[not_admin]"
+    redirect_to root_url
   end
 end
